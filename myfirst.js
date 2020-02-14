f9b889b26234bb4b14d8989f801d924499d3e10c
Eltom777 <lthomas.flynn@gmail.com>
2020-02-14 16:01:34 -0500

    Added busboy to parse the form sent by add.ejs

--- myfirst.js
+++ myfirst.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const firebase = require('firebase');
+const Busboy = require('busboy');
 
 firebase.initializeApp(config);
 var server = express();
@@ -14,6 +15,35 @@ server.get('/add.ejs',function(req,res){
   res.render('add');
 })
 server.post('/add.ejs',function(req,res){
-	
+  const Busboy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new Busboy({headers: req.headers});
+  
+  let imageToBeUploaded = {};
+  let imageFileName;
+  
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      console.log(mimetype);
+      
+      // my.image.png => ['my', 'image', 'png']
+      const imageExtension = filename.split('.')[filename.split('.').length - 1];
+      
+      // generate randome file id
+      imageFileName = `${Math.round(Math.random() * 1000000000000).toString()}.${imageExtension}`;
+
+      const filepath = path.join(os.tmpdir(), imageFileName);
+      imageToBeUploaded = { filepath, mimetype,imageFileName };
+      file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+      console.log(val);
+  });
+  busboy.on('finish', () => {
+	  
+  });
+  req.pipe(busboy);
 )}
 server.listen(8080);
