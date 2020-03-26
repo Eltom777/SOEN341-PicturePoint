b19814f77655d82224e6bd98fb34334d3e57153d
Eltom777 <lthomas.flynn@gmail.com>
2020-03-26 16:30:41 -0400

    Fixed mistakes not implemented when committing some features of  #85

--- picturepoint/src/Login/components/EmailUpdate.js
+++ picturepoint/src/Login/components/EmailUpdate.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { doEmailUpdate } from "../firebase/auth";
+import { doEmailUpdate } from "../../Firebase/functions/auth";
 
 const byPropKey = (propertyName, value) => () => ({
   [propertyName]: value
