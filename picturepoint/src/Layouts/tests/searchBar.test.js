698066c4b51a5692ad6f635ef645e39aa8ad1429
Anthony I <a.iacampo99@gmail.com>
2020-04-06 11:47:08 -0400

    Created a test to check invalid users #118

--- picturepoint/src/Layouts/tests/searchBar.test.js
+++ picturepoint/src/Layouts/tests/searchBar.test.js
@@ -1,11 +1,23 @@
 import { usernameExists } from "../../Firebase/functions/usernameExists";
 
-let username = "test123";
+const username = "test123";
+const invalidUsername = "test456";
 
 test('The user exists in the database', (done) => {
     usernameExists(username, (data) => {
         try {
             expect(data).toBeTruthy();
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
+
+test('The user does not exists in the database', (done) => {
+    usernameExists(invalidUsername, (data) => {
+        try {
+            expect(data).toBeFalsy();
             done();
         } catch (error) {
             done(error);
