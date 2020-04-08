f913023e3c62dd70eb8dd2ab01b1dd9fc5242957
JordanHum <jhum268@gmail.com>
2020-04-08 13:46:35 -0400

    added comments to describe the function for comments and likes

--- picturepoint/src/Firebase/functions/getComments.js
+++ picturepoint/src/Firebase/functions/getComments.js
@@ -1,6 +1,6 @@
 const { db } = require('./firebase');
 
-//Fetch one post
+//Fetch one post and all its comments
 export const getComments = (photoID, callback) => {
     db.collection('comments').orderBy('createdAt', 'desc').where('photo_id', '==', photoID).onSnapshot((snapshot) => {
         let comments = [];
