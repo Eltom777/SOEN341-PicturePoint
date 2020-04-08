18c0ea141ffb4060c0ae1103531a948bd7dbec8e
Eltom777 <lthomas.flynn@gmail.com>
2020-04-08 13:26:58 -0400

    Modified generateImageName with a recursive callback. Removed the progress callback inside addPhoto. Implement a callback to return if the picture was successfully uploaded. #123

--- picturepoint/src/Firebase/functions/addPhoto.js
+++ picturepoint/src/Firebase/functions/addPhoto.js
@@ -1,44 +1,51 @@
+//Firebase 
 import { db,storage } from "./firebase";
 
 const storageRef = storage.ref();
 
-export const addPhoto = (file,caption,progress) => {
-
-    let imageExtension = file.name.split('.')[file.name.split('.').length - 1];
-    let newImageName;
-    generateImageName(imageExtension, newImageName);
+export const addPhoto = (file,caption, callback) => {
   
-    //Upload file to google storage 
-    var uploadTask = storageRef.child(newImageName+"."+imageExtension).put(file);
-    uploadTask.on('state_changed', function(snapshot){
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-      }, function(error) {
-        // Handle unsuccessful uploads
-      }, function() {
-        // Handle successful uploads on complete
-        uploadTask.snapshot.ref.getDownloadURL().then(function(imageUrl) {
-            db.collection('/photos').doc(newImageName+"."+imageExtension).set({imageUrl,caption,user: localStorage.getItem("username"), creationDate: new Date().toISOString(), likes:0});
-            console.log('File available at', imageUrl);
-        });
-      });
+  let imageExtension = file.name.split('.')[file.name.split('.').length - 1];
+  let photoID;
+
+  generateImageName(imageExtension, (callback) =>{
+    photoID = callback+"."+imageExtension;
+  });
+
+  console.log(photoID);
+
+  //Upload file to google storage 
+  var uploadTask = storageRef.child(photoID).put(file);
+  uploadTask.on('state_changed', function(snapshot) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+    }, (error) => {
+      // Handle unsuccessful uploads
+      console.log("Error", error)
+      callback(null)
+    }, () => {
+      // Handle successful uploads on complete
+    uploadTask.snapshot.ref.getDownloadURL().then((imageUrl) => {
+      console.log('File available at', imageUrl);
+      db.collection('/photos').doc(photoID).set({imageUrl,caption,user: localStorage.getItem("username"), creationDate: new Date().toISOString(), likes:0});
+      callback(photoID)
+    });
+  });
+        
 };
 
-const generateImageName = (fileExtension, newName) => { //Checks if Image name already exists in DB
-  newName = `${Math.round(Math.random() * 1000000000000).toString()}.${fileExtension}`;
+const generateImageName = (fileExtension, callback) => { //Checks if Image name already exists in DB
+  callback(Math.round(Math.random() * 1000000000000).toString());
   
-  db.collection('photos').doc(newName+"."+fileExtension).get()
+  db.collection('photos').doc(callback+"."+fileExtension).get()
   .then(doc => {
     if(doc.exists){
       console.log("Image already exists, generating a new image name")
-      generateImageName(fileExtension, newName);
-   } 
+      generateImageName(fileExtension, (callbackRec) => {callback(callbackRec)});
+  } 
   })
   .catch(function(error) {
   console.log("Error: ", error);
   });
 }
-
-
-
