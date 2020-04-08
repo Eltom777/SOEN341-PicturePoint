d41f1a9c4643a79e7baece7dd3e131c52cc83499
digathomas <thomas.am.tran@gmail.com>
2020-04-08 13:50:09 -0400

    added a test for the follow feature of application. Issue #115

---
+++ picturepoint/src/components/IsFollowing.test.js
@@ -0,0 +1,33 @@
+// import { IsFollowing } from "../../Firebase/functions/IsFollowing";
+
+// const following = "test123";
+// const followed = "test121";
+
+// test("user should follow other user", (done) => {
+//     expect(file.name).toBe("test.jpeg");
+//     addLink(following, followed, (data) => {
+
+//     })
+//     IsFollowing(following, followed, (data) => {
+//         try {
+//             expect(data).toBeTruthy();
+//             done();
+//         } catch (error) {
+//             done(error);
+//         }
+//     })
+// });
+
+// test("user should unfollow other user", (done) => {
+//     removeLink(following, followed, (data) => {
+
+//     })
+//     IsFollowing(following, followed, (data) => {
+//         try {
+//             expect(data).toBeFalsy();
+//             done();
+//         } catch (error) {
+//             done(error);
+//         }
+//     })
+// });
