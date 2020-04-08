ab35fa2ca812189ac599e77be97201f10b06dec7
JordanHum <jhum268@gmail.com>
2020-04-08 13:48:06 -0400

    changed wording for getComments.js

--- picturepoint/src/Firebase/functions/getComments.js
+++ picturepoint/src/Firebase/functions/getComments.js
@@ -1,6 +1,6 @@
 const { db } = require('./firebase');
 
-//Fetch one post and all its comments
+//Fetch all the post's comments
 export const getComments = (photoID, callback) => {
     db.collection('comments').orderBy('createdAt', 'desc').where('photo_id', '==', photoID).onSnapshot((snapshot) => {
         let comments = [];
