dd864d72575852b12aa2c35abe80e618d8aecb83
Eltom777 <lthomas.flynn@gmail.com>
2020-04-08 21:59:49 -0400

    Updates

---
+++ precache-manifest.0b121d4675a72f86056cf2cc003bbf75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f0d3aaf13f8b75b73805b1336fa954",
+    "url": "/SOEN341-PicturePoint/picturepoint/index.html"
+  },
+  {
+    "revision": "85f34c452119eca6a7e3",
+    "url": "/SOEN341-PicturePoint/picturepoint/static/css/main.271135eb.chunk.css"
+  },
+  {
+    "revision": "b046ffc5ce969516c14a",
+    "url": "/SOEN341-PicturePoint/picturepoint/static/js/2.758c4608.chunk.js"
+  },
+  {
+    "revision": "e8c3d222977c637d31da635d6fc78957",
+    "url": "/SOEN341-PicturePoint/picturepoint/static/js/2.758c4608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f34c452119eca6a7e3",
+    "url": "/SOEN341-PicturePoint/picturepoint/static/js/main.ef51bbc7.chunk.js"
+  },
+  {
+    "revision": "4886ac43dee289ed1be4",
+    "url": "/SOEN341-PicturePoint/picturepoint/static/js/runtime-main.c59a01d7.js"
+  }
+]);
